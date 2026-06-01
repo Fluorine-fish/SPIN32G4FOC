055950fa1643237a754df18270aba5f0954d9bb0
胡文信 <atri@MacBook-Pro.local>
2026-06-01 14:58:24 +0800

    Init(FOC):First Init

---
+++ FOCApplication/App_DebugTask.h
@@ -0,0 +1,14 @@
+/**
+*   @file App_DebugTask.h
+*   @brief 
+*   @author Wenxin HU
+*   @date 2026/6/1
+*   @version 1.0
+*   @note
+*/
+#ifndef FOC_SPIING4_TEST_APP_DEBUGTASK_H
+#define FOC_SPIING4_TEST_APP_DEBUGTASK_H
+
+void App_DebugTask(void const * argument);
+
+#endif //FOC_SPIING4_TEST_APP_DEBUGTASK_H
